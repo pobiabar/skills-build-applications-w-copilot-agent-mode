d5c51b3fe7afe4309d56d7d59510082e7db341c7
Barbara Mrozek <barbara.mrozek@tieto.com>
2026-05-15 11:56:42 +0000

    backend changes

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,13 @@ SECRET_KEY = 'django-insecure-2mte2z*!dqy=nsg!!ocy(u)*d9xq#h*77t+)0t!%tq0m6m3d7_
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+
+import os
+# Allow localhost and Codespace public URL
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
